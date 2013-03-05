e74c7e23b51e082c48b3bded843df44c66e1b8be
Chris Zarate <czarate@mla.org>
2013-03-05 15:01:37 -0500

    Simplify selection grabbing for Firefox compatibility.

--- js/sumter.js
+++ js/sumter.js
@@ -61,71 +61,36 @@
       ]
     },
     // Placeholder for current selection
-    selection: {},
-    // Placeholder for current totals.
-    totals: ''
+    selection: ''
   };
 
-  // Make a little total box that's easy to find.
-  var styleAttr = document.createAttribute('style');
-      styleAttr.value = sumter.config.styles.join('');
-  var sumterDialog = document.createElement('div');
-      sumterDialog.setAttributeNode(styleAttr);
-      sumterDialog.appendChild(document.createTextNode(sumter.config.title));
-  document.body.appendChild(sumterDialog);
+  // Save reference to UI text node.
+  sumter.hud = document.createTextNode(sumter.config.title);
+
+  // Make a little box that's easy to find.
+  var sumterEl = document.createElement('div'),
+      sumterStyleAttr = document.createAttribute('style');
+  sumterStyleAttr.value = sumter.config.styles.join('');
+  sumterEl.appendChild(sumter.hud);
+  sumterEl.setAttributeNode(sumterStyleAttr);
+  document.body.appendChild(sumterEl);
 
   // Make a rounder for the desired number of decimal places.
   for(var i = 0, rounder = '1'; i < sumter.config.round; i++) rounder += '0';
   sumter.config.rounder = parseInt(rounder, 10);
 
-  // Get selection object or selection text string.
+  // Get selection text.
   function grabSelection() {
     if(window.getSelection) {
-      return window.getSelection();
+      return window.getSelection().toString();
     } else if(document.getSelection) {
-      return document.getSelection();
+      return document.getSelection().toString();
     } else if(document.selection) {
       return document.selection.createRange().text;
     } else return '';
   }
 
-  // Get all *text* nodes contained in a selection object.
-  // Adapted from code by Tim Down.
-  // http://stackoverflow.com/questions/4398526/how-can-i-find-all-text-nodes-between-to-element-nodes-with-javascript-jquery
-  function getTextNodesBetween(selection) {
-    var range = selection.getRangeAt(0), rootNode = range.commonAncestorContainer,
-        startNode = range.startContainer, endNode = range.endContainer,
-        startOffset = range.startOffset, endOffset = range.endOffset,
-        pastStartNode = false, reachedEndNode = false, textNodes = [];
-    function getTextNodes(node) {
-      var val = node.nodeValue;
-      if(node == startNode && node == endNode && node !== rootNode) {
-        if(val) textNodes.push(val.substring(startOffset, endOffset));
-        pastStartNode = reachedEndNode = true;
-      } else if(node == startNode) {
-        if(val) textNodes.push(val.substring(startOffset));
-        pastStartNode = true;
-      } else if(node == endNode) {
-        if(val) textNodes.push(val.substring(0, endOffset));
-        reachedEndNode = true;
-      } else if(node.nodeType == 3) {
-        if(val && pastStartNode && !reachedEndNode && !/^\s*$/.test(val)) {
-          textNodes.push(val);
-        }
-      }
-      for(var i = 0, len = node.childNodes.length; !reachedEndNode && i < len; ++i) {
-        if(node !== sumterDialog) getTextNodes(node.childNodes[i]);
-      }
-    }
-    getTextNodes(rootNode);
-    return textNodes;
-  }
-
-  // Loop through nodes.
-  function processNodes(arr, totals) {
-    for(var i = 0; i < arr.length; i++) totals = processText(arr[i], totals);
-    return totals;
-  }
+  // Loop through text.
   function processText(text, totals) {
     var arr = text.split(/\s+/);
     for(var i = 0, sum = 0; i < arr.length; i++) {
@@ -147,16 +112,6 @@
       }
     }
     return totals;
-  }
-
-  // Lazy Range object detection.
-  function isRange(obj) {
-    return ('type' in obj && obj.type === 'Range');
-  }
-
-  // Good-enough Range object comparison.
-  function rangeChange(data1, data2) {
-    return (data1.type !== data2.type || data1.focusNode !== data2.focusNode || data1.focusOffset !== data2.focusOffset);
   }
 
   // Round a number. *Glares at parseFloat.*
@@ -186,33 +141,18 @@
     // Grab text selection.
     var sel = grabSelection();
 
-    // Test for something usable.
-    switch(typeof sel) {
-      case 'object':
-        // is selection object : TRAVERSE DAT DOM HOMIE
-        if(isRange(sel)){
-          if(rangeChange(sel, sumter.selection)) {
-            sumterDialog.innerHTML = sumter.total = tallyTotal(processNodes(getTextNodesBetween(sel), []));
-            sumter.selection = { type: sel.type, focusNode: sel.focusNode, focusOffset: sel.focusOffset };
-          }
-          return false;
-        }
-        break;
-      case 'string':
-        // is selection string : FALLBACK, DO DUMB STUFF
-        if(sel !== '') {
-          if(sel !== sumter.selection) {
-            sumterDialog.innerHTML = sumter.total = tallyTotal(processText(sel, []));
-            sumter.selection = sel + '';
-          }
-          return false;
-        }
-        break;
+    // Parse text.
+    if(sel !== '') {
+      if(sel !== sumter.selection) {
+        sumter.hud.nodeValue = sumter.total = tallyTotal(processText(sel, []));
+        sumter.selection = sel + '';
+      }
+      return false;
     }
 
     // empty selection or selection too young : RESET
     if(sumter.total !== sumter.config.title) {
-      sumterDialog.innerHTML = sumter.total = sumter.config.title;
+      sumter.hud.nodeValue = sumter.total = sumter.config.title;
     }
 
   };
