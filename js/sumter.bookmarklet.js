e9234082d8def75f4a34da01d5d00074f3bb85cf
Chris Zarate <chris@zarate.org>
2013-03-04 19:24:36 -0500

    Changed hosted URL to CloudFront for flexible HTTPS loading.

--- js/sumter.bookmarklet.js
+++ js/sumter.bookmarklet.js
@@ -1,8 +1,9 @@
 /*
  * Sumter
  * https://github.com/chriszarate/Sumter
+ * d391j46upa8a0p.cloudfront.net == files.zarate.org
  */
 
 var a=document.createElement('script');
-    a.setAttribute('src','http://files.zarate.org/js/sumter.js?updated');
+    a.setAttribute('src','//d391j46upa8a0p.cloudfront.net/js/sumter.js?updated');
 document.getElementsByTagName('head')[0].appendChild(a);
